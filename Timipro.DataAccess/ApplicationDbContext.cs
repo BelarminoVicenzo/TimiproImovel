c68ecbf02e67750a3d6e106113e6097b9148e5a0
Belarmino Vicenzo <belarmino.lp@hotmail.com>
2021-08-18 07:44:52 +0100

    Created Data Access class library project

---
+++ Timipro.DataAccess/ApplicationDbContext.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using TimiProImovel.Models;
+
+namespace Timipro.DataAccess
+{
+    public class ApplicationDbContext : DbContext
+    {
+
+        public ApplicationDbContext() : base("name=TimiproImovel")
+        {
+            //prevent creating a new db 77
+            Database.SetInitializer<ApplicationDbContext>(null);
+        }
+
+        public DbSet<Cliente> Cliente { get; set; }
+        public DbSet<Imovel> Imovel { get; set; }
+        public DbSet<TipoNegocio> TipoNegocio { get; set; }
+        public DbSet<ClienteImovel> ClienteImovel { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            //for plural names, this will prevent
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+
+    
+
+
+    }
+
+
+}
---
+++ Timipro.DataAccess/Migrations/202108180627010_initial.cs
@@ -0,0 +1,77 @@
+﻿namespace Timipro.DataAccess.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class initial : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Cliente",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        CPF = c.String(maxLength: 11),
+                        Nome = c.String(nullable: false),
+                        Email = c.String(nullable: false, maxLength: 80),
+                        Ativo = c.Boolean(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .Index(t => t.Email, unique: true);
+            
+            CreateTable(
+                "dbo.ClienteImovel",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        IdCliente = c.Int(nullable: false),
+                        IdImovel = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Imovel", t => t.IdImovel, cascadeDelete: true)
+                .ForeignKey("dbo.Cliente", t => t.IdCliente, cascadeDelete: true)
+                .Index(t => t.IdCliente)
+                .Index(t => t.IdImovel);
+            
+            CreateTable(
+                "dbo.Imovel",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Descricao = c.String(),
+                        Valor = c.Single(nullable: false),
+                        Ativo = c.Boolean(nullable: false),
+                        IdTipoNegocio = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.TipoNegocio", t => t.IdTipoNegocio, cascadeDelete: true)
+                .Index(t => t.IdTipoNegocio);
+            
+            CreateTable(
+                "dbo.TipoNegocio",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Tipo = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.ClienteImovel", "IdCliente", "dbo.Cliente");
+            DropForeignKey("dbo.ClienteImovel", "IdImovel", "dbo.Imovel");
+            DropForeignKey("dbo.Imovel", "IdTipoNegocio", "dbo.TipoNegocio");
+            DropIndex("dbo.Imovel", new[] { "IdTipoNegocio" });
+            DropIndex("dbo.ClienteImovel", new[] { "IdImovel" });
+            DropIndex("dbo.ClienteImovel", new[] { "IdCliente" });
+            DropIndex("dbo.Cliente", new[] { "Email" });
+            DropTable("dbo.TipoNegocio");
+            DropTable("dbo.Imovel");
+            DropTable("dbo.ClienteImovel");
+            DropTable("dbo.Cliente");
+        }
+    }
+}
