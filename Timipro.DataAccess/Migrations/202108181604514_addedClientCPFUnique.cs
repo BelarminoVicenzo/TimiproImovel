fb2fe2eb9f45959b99ac429e71b861c7df75e3de
Belarmino Vicenzo <belarmino.lp@hotmail.com>
2021-08-18 17:11:56 +0100

    Added migration to add ClienteCPF uniqe

---
+++ Timipro.DataAccess/Migrations/202108181604514_addedClientCPFUnique.cs
@@ -0,0 +1,20 @@
+﻿namespace Timipro.DataAccess.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class addedClientCPFUnique : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Cliente", "CPF", c => c.String(nullable: false, maxLength: 11));
+            CreateIndex("dbo.Cliente", "CPF", unique: true);
+        }
+        
+        public override void Down()
+        {
+            DropIndex("dbo.Cliente", new[] { "CPF" });
+            AlterColumn("dbo.Cliente", "CPF", c => c.String(maxLength: 11));
+        }
+    }
+}
