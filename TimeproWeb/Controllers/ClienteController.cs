e0f15dc931f9bf3dca4066b1e6ceebb53362c15c
Belarmino Vicenzo <belarmino.lp@hotmail.com>
2021-08-19 08:08:16 +0100

    created Cliente controller and views

---
+++ TimeproWeb/Controllers/ClienteController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+using Timipro.BLL.Services;
+using Timipro.Models;
+
+namespace TimiproImovel.Controllers
+{
+    public class ClienteController : Controller
+    {
+        private ClienteService _clienteService;
+       
+
+        public ClienteController(ClienteService clienteService)
+        {
+            _clienteService = clienteService;
+                  }
+        // GET: Cliente
+        public async Task<ActionResult> Index()
+        {
+
+            return View(await _clienteService.GetAll());
+        }
+
+       
+        public async Task<ActionResult> Details(int id)
+        {
+            var cliente = await _clienteService.Get(id);
+            return View(cliente);
+        }
+
+
+        
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create(Cliente cliente)
+        {
+            try
+            {
+                await _clienteService.Create(cliente);
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        
+        public async Task<ActionResult> Edit(int id)
+        {
+            var cliente = await _clienteService.Get(id);   
+            return View(cliente);
+        }
+
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit(Cliente cliente)
+        {
+            try
+            {
+
+                await _clienteService.Update(cliente);
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                return View(ex.Message);
+            }
+        }
+
+       
+        public async Task<ActionResult> Delete(int id)
+        {
+            var cliente = await _clienteService.Get(id);
+            return View(cliente);
+        }
+
+       
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Delete(Cliente cliente)
+        {
+            try
+            {
+
+                await _clienteService.Delete(cliente);
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                return View(ex.Message);
+            }
+        }
+    }
+}
